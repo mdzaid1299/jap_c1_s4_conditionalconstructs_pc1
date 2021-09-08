9cafaee86cf727471423983395de84c48204f607
bhakti.rathod <bhakti.rathod@stackroute.in>
2021-09-08 10:58:36 +0530

    Sprint4 PC1 added

---
+++ src/main/java/com/jap/cricketscoremanagement/CricketScore.java
@@ -0,0 +1,17 @@
+package com.jap.cricketscoremanagement;
+
+
+
+public class CricketScore {
+  /**
+   *  Function accepts scores of 3 teams as input parameters and identifies the
+   *  team which is winner of the series.
+   */
+
+  public String winnerOfSeriesCalculation(int totalScore1, int totalScore2, int totalScore3) {
+
+      return "";
+  }
+
+  
+}
