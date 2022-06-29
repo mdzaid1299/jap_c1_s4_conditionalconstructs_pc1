4faf6585e03efdabe9b6f62273b0ce316bf33d32
mdzaid1299 <mdzid1299@gmail.com>
2022-06-29 14:23:56 +0530

    Completed CricketScore () Method

--- src/main/java/com/jap/cricketscoremanagement/CricketScore.java
+++ src/main/java/com/jap/cricketscoremanagement/CricketScore.java
@@ -1,17 +1,17 @@
 package com.jap.cricketscoremanagement;
 
 
-
 public class CricketScore {
-  /**
-   *  Function accepts scores of 3 teams as input parameters and identifies the
-   *  team which is winner of the series.
-   */
+    /**
+     * Function accepts scores of 3 teams as input parameters and identifies the
+     * team which is winner of the series.
+     */
 
-  public String winnerOfSeriesCalculation(int totalScore1, int totalScore2, int totalScore3) {
+    public String winnerOfSeriesCalculation(int totalScore1, int totalScore2, int totalScore3) {
+        String winner = (totalScore1 > totalScore2) ? ((totalScore1 > totalScore3) ? "Australia is winner" : "New Zealand is winner") : (totalScore2 > totalScore3) ? "England is winner" : "New Zealand is winner";
 
-      return "";
-  }
+        return winner;
+    }
 
-  
+
 }
--- src/test/java/com/jap/cricketscoremanagement/CricketScoreTest.java
+++ src/test/java/com/jap/cricketscoremanagement/CricketScoreTest.java
@@ -1,47 +1,47 @@
 package com.jap.cricketscoremanagement;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class CricketScoreTest {
 
-  CricketScore cricketScore;
+    CricketScore cricketScore;
 
-  @BeforeEach
-  void init() {
-    cricketScore = new CricketScore();
-  }
+    @BeforeEach
+    void init() {
+        cricketScore = new CricketScore();
+    }
 
-  // Test winnerOfSeriesCalculation method by passing scores of 3 teams.
-  @Test
-  public void givenScoresThenReturnWinner() {
-    assertEquals("England is winner", cricketScore.winnerOfSeriesCalculation(21, 24, 20));
-    assertEquals("Australia is winner", cricketScore.winnerOfSeriesCalculation(29, 15, 28));
-    assertEquals("New Zealand is winner", cricketScore.winnerOfSeriesCalculation(20, 25, 30));
-  }
+    // Test winnerOfSeriesCalculation method by passing scores of 3 teams.
+    @Test
+    public void givenScoresThenReturnWinner() {
+        assertEquals("England is winner", cricketScore.winnerOfSeriesCalculation(21, 24, 20));
+        assertEquals("Australia is winner", cricketScore.winnerOfSeriesCalculation(29, 15, 28));
+        assertEquals("New Zealand is winner", cricketScore.winnerOfSeriesCalculation(20, 25, 30));
+    }
 
-  // Test winnerOfSeriesCalculation method by passing negative scores for few
-  // teams.
-  @Test
-  public void givenNegativeScoresThenReturnWinner() {
-    assertNotEquals("England is winner", cricketScore.winnerOfSeriesCalculation(21, -24, 20));
-    assertEquals("Australia is winner", cricketScore.winnerOfSeriesCalculation(21, -24, 20));
+    // Test winnerOfSeriesCalculation method by passing negative scores for few
+    // teams.
+    @Test
+    public void givenNegativeScoresThenReturnWinner() {
+        assertNotEquals("England is winner", cricketScore.winnerOfSeriesCalculation(21, -24, 20));
+        assertEquals("Australia is winner", cricketScore.winnerOfSeriesCalculation(21, -24, 20));
 
-    assertNotEquals("Australia is the winner", cricketScore.winnerOfSeriesCalculation(-29, 15, 28));
-    assertEquals("New Zealand is winner", cricketScore.winnerOfSeriesCalculation(-29, 15, 28));
+        assertNotEquals("Australia is the winner", cricketScore.winnerOfSeriesCalculation(-29, 15, 28));
+        assertEquals("New Zealand is winner", cricketScore.winnerOfSeriesCalculation(-29, 15, 28));
 
-    assertNotEquals("New Zealand is the winner", cricketScore.winnerOfSeriesCalculation(20, 25, -30));
-    assertEquals("England is winner", cricketScore.winnerOfSeriesCalculation(20, 25, -30));
-  }
+        assertNotEquals("New Zealand is the winner", cricketScore.winnerOfSeriesCalculation(20, 25, -30));
+        assertEquals("England is winner", cricketScore.winnerOfSeriesCalculation(20, 25, -30));
+    }
 
-  // Test winnerOfSeriesCalculation method by passing scores as 0 for few teams.
-  @Test
-  public void givenScoresAsZeroThenReturnWinner() {
-    assertEquals("England is winner", cricketScore.winnerOfSeriesCalculation(0, 24, 20));
-    assertEquals("Australia is winner", cricketScore.winnerOfSeriesCalculation(29, 0, 28));
-    assertEquals("England is winner", cricketScore.winnerOfSeriesCalculation(20, 25, 0));
-  }
+    // Test winnerOfSeriesCalculation method by passing scores as 0 for few teams.
+    @Test
+    public void givenScoresAsZeroThenReturnWinner() {
+        assertEquals("England is winner", cricketScore.winnerOfSeriesCalculation(0, 24, 20));
+        assertEquals("Australia is winner", cricketScore.winnerOfSeriesCalculation(29, 0, 28));
+        assertEquals("England is winner", cricketScore.winnerOfSeriesCalculation(20, 25, 0));
+    }
 }
